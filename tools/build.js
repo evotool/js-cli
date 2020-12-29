3cf5c5f4348b9db72a55ace5734dc5539e87f767
Shamil Sultanov <shamil.amirovich@gmail.com>
2020-12-29 20:25:25 +0300

    tools/build: update

--- tools/build.js
+++ tools/build.js
@@ -11,7 +11,13 @@ delete packageJson.jest;
 delete packageJson.nodemonConfig;
 delete packageJson.devDependencies;
 
-writeFileSync(resolve(mainDir, 'dist/package.json'), JSON.stringify(packageJson, null, 2));
+writeFileSync(resolve(mainDir, 'dist/package.json'), JSON.stringify(packageJson, null, '\t'));
+
+const jsTsFiles = sync(resolve(__dirname, '../dist/**/*.{js,ts}'));
+
+for (const f of jsTsFiles) {
+	writeFileSync(f, readFileSync(f, 'utf-8').replace(/ {4}/g, '\t'));
+}
 
 const copyingFiles = ['LICENSE', 'README.md', ...sync(resolve(mainDir, 'templates/**'), { nodir: true, dot: true })].map((p) => resolve(p).substring(mainDir.length + 1));
 
